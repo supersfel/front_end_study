4a9e724d23568aeb4a5b209396223621e3a3e2b5
supersfel <jmg081005@naver.com>
2022-01-02 22:10:24 +0900

    0~2

---
+++ 0_Homework/5week/exam/5/app.js
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+const $html = document.querySelector("#app");
+
+$html.innerHTML = `
+<ul class="navigation">
+<li class="menu-item">menu1</li>
+<li class="menu-item">menu2</li>
+<li class="menu-item">menu3</li>
+<li class="menu-item">menu4</li>
+<li class="menu-item">menu5</li>
+</ul>
+`;
---
+++ 0_Homework/5week/src/index.js
@@ -0,0 +1,9 @@
+import "./styles.scss";
+
+document.getElementById("app").innerHTML = `
+<div><a href="../exam/1/index.html">1번</a><div>
+<div><a href="../exam/2/index.html">2번</a><div>
+<div><a href="../exam/3/index.html">3번</a><div>
+<div><a href="../exam/4/index.html">4번</a><div>
+<div><a href="../exam/5/index.html">5번</a><div>
+`;
