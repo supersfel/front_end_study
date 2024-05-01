aeb67f257d72f9e7ea5a4639d57fd10bd29a7349
정민규 <jmg081005@naver.com>
2024-05-01 20:08:50 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,7 @@
-ary = ["정민규", "양홍주", "김유나"];
 
-const dfs = (n, ret) => {
-  if (n === 3) {
-    st = "";
-    for (const a of ret) {
-      st += a;
-    }
-    console.log(st);
-    return;
-  }
 
-  for (let i = 0; i < 3; i++) {
-    dfs(n + 1, [...ret, ary[ret.length][i]]);
-  }
-};
+Webpack -> js 3
+ 
+vite -> Go 2
 
-dfs(0, []);
+turbopack -> Rust 1
